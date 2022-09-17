cb7eff4f005a9a63e266424805e9166ae6a29c89
sohel17sheikh <sohel17sheikh@gmail.com>
2022-09-17 12:48:57 +0530

    Interface Added

---
+++ E_com/src/com/kart/bash/ecom/mod/submod/Bash_Kart.java
@@ -0,0 +1,21 @@
+package com.kart.bash.ecom.mod.submod;
+
+public interface Bash_Kart {
+
+	public void userVerification();
+
+	public void createProductDatabase();
+
+	public void purchase();
+
+	public void showProducts();
+
+	public void addedToCart();
+
+	public void checkOutDetails();
+
+	public void userRegistery();
+
+	public void orderHistory();
+
+}
